ced8684d155054b4389759bbca190682dbe1a2cd
khanhnguyen <khanhnv@apero.vn>
2025-12-29 10:35:12 +0700

    update ver lib 1.0.22

--- MetaBrain/build.gradle.kts
+++ MetaBrain/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
 
                 groupId = "com.github.quanvh"
                 artifactId = "metabrain"
-                version = "1.0.21"
+                version = "1.0.22"
             }
         }
         repositories {
