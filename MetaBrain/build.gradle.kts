154ff3b48e4a254b05f97d6afe8d0ea2786edc35
khanhnguyen <khanhnv@apero.vn>
2026-01-06 18:05:07 +0700

    update library version to 1.0.25

--- MetaBrain/build.gradle.kts
+++ MetaBrain/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
 
                 groupId = "com.github.quanvh"
                 artifactId = "metabrain"
-                version = "1.0.24"
+                version = "1.0.25"
             }
         }
         repositories {
